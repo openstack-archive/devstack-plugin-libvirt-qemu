aabaadb17d3527651dd1cb1e5898676103d219c8
Rodolfo Alonso Hernandez <rodolfo.alonso.hernandez@intel.com>
2017-05-24 17:18:00 +0100

    Initial release
    This commit contains the scripts to download from source QEMU
    and libvirt, specify a specific commit ID and apply on top
    of it a list of patches.
    
    Change-Id: Id52690f4f175c990f0b481c900a30087c07c0c31

---
+++ devstack/plugin.sh
@@ -0,0 +1,53 @@
+local xtrace=$(set +o | grep xtrace)
+if [ "$VERBOSE" == 'True' ]; then
+    # enabling verbosity on whole plugin - default behavior
+    set -o xtrace
+fi
+
+local error_on_clone=${ERROR_ON_CLONE}
+# disabling ERROR_NO_CLONE to allow this plugin work with devstack-gate
+ERROR_ON_CLONE=False
+
+    # Initial source of lib script
+    source $DEVSTACK_LIBVIRT_QEMU_PLUGIN_DIR/devstack/libs/libvirt
+
+    case $1 in
+        "stack")
+            case $2 in
+                "pre-install")
+                    # cloning source code
+                    if [ $LIBVIRT_QEMU_SRC_INSTALL == 'True' ]; then
+                        echo_summary "Downloading source repos for libvirt and qemu"
+                        clone_libvirt_qemu
+                    else
+                        echo_summary "Cloning of src files for libvirt and qemu disabled"
+                    fi
+                ;;
+                "install")
+                    # no-op
+                    # install called via by devstack/lib/nova
+                    :
+                ;;
+                "post-config")
+                    # no-op
+                    :
+                ;;
+                "extra")
+                    # no-op
+                    :
+                ;;
+            esac
+        ;;
+        "unstack")
+            # Shut Down OVS-DPDK
+            uninstall_libvirt_qemu
+        ;;
+        "clean")
+            # Remove state and transient data
+            # Remember clean.sh first calls unstack.sh
+            cleanup_libvirt_qemu_source
+        ;;
+    esac
+
+ERROR_ON_CLONE=$error_on_clone
+$xtrace
---
+++ releasenotes/source/index.rst
@@ -0,0 +1,8 @@
+============================================
+ devstack_plugin_libvirt_qemu Release Notes
+============================================
+
+.. toctree::
+   :maxdepth: 1
+
+   unreleased
